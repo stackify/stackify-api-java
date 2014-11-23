86474f3207e39758c68bcbc94734550e2623feb1
blackopsdev <pavel@blackopsdev.com>
2014-11-24 02:03:36 +0300

    non-existing param removed, redundant tabs were removed

--- src/main/java/com/stackify/api/common/log/LogBackgroundServiceScheduler.java
+++ src/main/java/com/stackify/api/common/log/LogBackgroundServiceScheduler.java
@@ -23,93 +23,92 @@ import com.stackify.api.common.http.HttpException;
 
 /**
  * LogBackgroundServiceScheduler
- * 
+ *
  * @author Eric Martin
  */
 public class LogBackgroundServiceScheduler extends CustomScheduler {
-		
+
 	/**
 	 * One second (milliseconds)
 	 */
 	private static final long ONE_SECOND = 1000;
-	
+
 	/**
 	 * Five seconds (milliseconds)
 	 */
 	private static final long FIVE_SECONDS = 5000;
-	
+
 	/**
 	 * One minute (milliseconds)
 	 */
 	private static final long ONE_MINUTE = 60000;
-	
+
 	/**
 	 * Five minutes (milliseconds)
 	 */
 	private static final long FIVE_MINUTES = 300000;
-	
+
 	/**
 	 * Schedule delay (in milliseconds)
 	 */
 	private long scheduleDelay = ONE_SECOND;
-	
+
 	/**
 	 * UTC timestamp of the HTTP error
 	 */
 	private long lastHttpError = 0;
-	
+
 	/**
 	 * Sets the next scheduled delay based on the number of messages sent
 	 * @param numSent The number of log messages sent
 	 */
 	public void update(final int numSent) {
-		
+
 		// Reset the last HTTP error
-			
+
 		lastHttpError = 0;
-		
+
 		// adjust the schedule delay based on the number of messages sent in the last iteration
-		
+
 		if (100 <= numSent) {
-			
+
 			// messages are coming in quickly so decrease our delay
 			// minimum delay is 1 second
-			
+
 			scheduleDelay = Math.max(Math.round(scheduleDelay / 2.0), ONE_SECOND);
-			
+
 		} else if (numSent < 10) {
-			
+
 			// messages are coming in rather slowly so increase our delay
 			// maximum delay is 5 seconds
-			
+
 			scheduleDelay = Math.min(Math.round(1.25 * scheduleDelay), FIVE_SECONDS);
-			
+
 		}
 	}
 
 	/**
 	 * Sets the next scheduled delay based on the HTTP transmission status
 	 * @param t The exception
-	 * @param status The HTTP transmission status
 	 */
 	public void update(final Throwable t) {
-		
+
 		// see if the exception indicates an authorization problem
-		
+
 		boolean isAuthorized = true;
-		
+
 		if (t instanceof HttpException) {
 			HttpException httpException = (HttpException) t;
-			
+
 			if (httpException.getStatusCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
 				isAuthorized = false;
 			}
 		}
-		
-		// adjust the schedule delay based on the type of error 
-		
+
+		// adjust the schedule delay based on the type of error
+
 		if (isAuthorized) {
-			
+
 			// Set the last HTTP error (if not already set)
 
 			if (lastHttpError == 0) {
@@ -121,19 +120,19 @@ public class LogBackgroundServiceScheduler extends CustomScheduler {
 			// Max schedule delay = 1 minute
 
 			long sinceFirstError = System.currentTimeMillis() - lastHttpError;
-			
+
 			scheduleDelay = Math.min(Math.max(sinceFirstError, ONE_SECOND), ONE_MINUTE);
-			
+
 		} else {
-			
+
 			// Set the last HTTP error
 			// Set the schedule delay to five minutes
-			
+
 			lastHttpError = System.currentTimeMillis();
 			scheduleDelay = FIVE_MINUTES;
 		}
 	}
-	
+
 	/**
 	 * @return the scheduleDelay
 	 */
