53f6294b9714e54b470254653cd97272940d115c
Darin Howard <dhoward@stackify.com>
2018-08-08 11:32:52 -0500

    RT-987 - adding in Stackify RUM client code.

---
+++ src/main/java/com/stackify/apm/Stackify.java
@@ -0,0 +1,20 @@
+package com.stackify.apm;
+
+/**
+ * Stackify Utility.
+ * Provides function to manually instrument web requests for RUM tracking.
+ *
+ * @author Darin Howard
+ */
+public class Stackify {
+
+    /**
+     * Returns the RUM JavaScript block for tracking the current web request
+     *
+     * @return String javascript script block
+     */
+    public static String getRUMJavaScriptBlock() {
+        return "";
+    }
+
+}
