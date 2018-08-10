b7613d85347244ad305a9bf84900f2177e7090c2
Darin Howard <dhoward@stackify.com>
2018-08-10 10:19:29 -0500

    Updated LogMsg to include id in builder - allowing for manual setting of value.

--- src/main/java/com/stackify/api/LogMsg.java
+++ src/main/java/com/stackify/api/LogMsg.java
@@ -15,9 +15,6 @@
  */
 package com.stackify.api;
 
-import java.util.List;
-import java.util.UUID;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -25,8 +22,12 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+import java.util.UUID;
+
 /**
  * LogMsg
+ *
  * @author Eric Martin
  */
 @Getter
@@ -34,175 +35,182 @@ import lombok.Setter;
 @JsonDeserialize(builder = LogMsg.Builder.class)
 public class LogMsg {
 
-	/**
-	 * The log message
-	 */
-	@Setter
-	@JsonProperty("Msg")
-	private String msg;
+    /**
+     * The log message
+     */
+    @Setter
+    @JsonProperty("Msg")
+    private String msg;
 
-	/**
-	 * Extra contextual data from the log message
-	 */
-	@Setter
-	@JsonProperty("data")
-	private String data;
+    /**
+     * Extra contextual data from the log message
+     */
+    @Setter
+    @JsonProperty("data")
+    private String data;
 
-	/**
-	 * The error/exception details
-	 */
-	@JsonProperty("Ex")
-	private final StackifyError ex;
+    /**
+     * The error/exception details
+     */
+    @JsonProperty("Ex")
+    private final StackifyError ex;
 
-	/**
-	 * The thread name
-	 */
-	@JsonProperty("Th")
-	private final String th;
+    /**
+     * The thread name
+     */
+    @JsonProperty("Th")
+    private final String th;
 
-	/**
-	 * Unix timestamp of the log message
-	 */
-	@JsonProperty("EpochMs")
-	private final Long epochMs;
+    /**
+     * Unix timestamp of the log message
+     */
+    @JsonProperty("EpochMs")
+    private final Long epochMs;
 
-	/**
-	 * Log level of the message
-	 */
-	@JsonProperty("Level")
-	private final String level;
+    /**
+     * Log level of the message
+     */
+    @JsonProperty("Level")
+    private final String level;
 
-	/**
-	 * Transaction id
-	 */
-	@JsonProperty("TransID")
-	private final String transId;
+    /**
+     * Transaction id
+     */
+    @JsonProperty("TransID")
+    private final String transId;
 
-	/**
-	 * Source method name
-	 */
-	@JsonProperty("SrcMethod")
-	private final String srcMethod;
+    /**
+     * Source method name
+     */
+    @JsonProperty("SrcMethod")
+    private final String srcMethod;
 
-	/**
-	 * Source line number
-	 */
-	@JsonProperty("SrcLine")
-	private final Integer srcLine;
+    /**
+     * Source line number
+     */
+    @JsonProperty("SrcLine")
+    private final Integer srcLine;
 
-	/**
-	 * The log message id
-	 */
-	@JsonProperty("id")
-	private final String id;
+    /**
+     * The log message id
+     */
+    @JsonProperty("id")
+    private final String id;
 
     /**
      * List of Tags
      */
     @JsonProperty("Tags")
-	private final List<String> tags;
+    private final List<String> tags;
 
 
-	/**
-	 * @return An instance of Builder, based on current class state
-	 */
-	public Builder toBuilder() {
-		return newBuilder()
-			.msg(this.msg)
-			.data(this.data)
-			.ex(this.ex)
-			.th(this.th)
-			.epochMs(this.epochMs)
-			.level(this.level)
-			.transId(this.transId)
-			.srcMethod(this.srcMethod)
-			.srcLine(this.srcLine)
-            .tags(this.tags);
-	}
+    /**
+     * @return An instance of Builder, based on current class state
+     */
+    public Builder toBuilder() {
+        return newBuilder()
+                .id(this.id)
+                .msg(this.msg)
+                .data(this.data)
+                .ex(this.ex)
+                .th(this.th)
+                .epochMs(this.epochMs)
+                .level(this.level)
+                .transId(this.transId)
+                .srcMethod(this.srcMethod)
+                .srcLine(this.srcLine)
+                .tags(this.tags);
+    }
 
-	/**
-	 * @param builder The Builder object that contains all of the values for initialization
-	 */
-	private LogMsg(final Builder builder) {
-		this.msg = builder.msg;
-		this.data = builder.data;
-		this.ex = builder.ex;
-		this.th = builder.th;
-		this.epochMs = builder.epochMs;
-		this.level = builder.level;
-		this.transId = builder.transId;
-		this.srcMethod = builder.srcMethod;
-		this.srcLine = builder.srcLine;
-		this.id = UUID.randomUUID().toString();
+    /**
+     * @param builder The Builder object that contains all of the values for initialization
+     */
+    private LogMsg(final Builder builder) {
+        this.id = builder.id != null ? builder.id : UUID.randomUUID().toString();
+        this.msg = builder.msg;
+        this.data = builder.data;
+        this.ex = builder.ex;
+        this.th = builder.th;
+        this.epochMs = builder.epochMs;
+        this.level = builder.level;
+        this.transId = builder.transId;
+        this.srcMethod = builder.srcMethod;
+        this.srcLine = builder.srcLine;
         this.tags = builder.tags;
-	}
+    }
 
-	/**
-	 * @return A new instance of the Builder
-	 */
-	public static Builder newBuilder() {
-	    return new Builder();
-	}
+    /**
+     * @return A new instance of the Builder
+     */
+    public static Builder newBuilder() {
+        return new Builder();
+    }
 
-	/**
-	 * LogMsg.Builder separates the construction of a LogMsg from its representation
-	 */
-	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Builder {
+    /**
+     * LogMsg.Builder separates the construction of a LogMsg from its representation
+     */
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Builder {
 
-		/**
-		 * The builder's msg
-		 */
-		@JsonProperty("Msg")
-		private String msg;
+        /**
+         * The log message id
+         */
+        @JsonProperty("id")
+        private String id;
 
-		/**
-		 * The builder's data
-		 */
-		@JsonProperty("data")
-		private String data;
+        /**
+         * The builder's msg
+         */
+        @JsonProperty("Msg")
+        private String msg;
 
-		/**
-		 * The builder's ex
-		 */
-		@JsonProperty("Ex")
-		private StackifyError ex;
+        /**
+         * The builder's data
+         */
+        @JsonProperty("data")
+        private String data;
 
-		/**
-		 * The builder's th
-		 */
-		@JsonProperty("Th")
-		private String th;
+        /**
+         * The builder's ex
+         */
+        @JsonProperty("Ex")
+        private StackifyError ex;
 
-		/**
-		 * The builder's epochMs
-		 */
-		@JsonProperty("EpochMs")
-		private Long epochMs;
+        /**
+         * The builder's th
+         */
+        @JsonProperty("Th")
+        private String th;
 
-		/**
-		 * The builder's level
-		 */
-		@JsonProperty("Level")
-		private String level;
+        /**
+         * The builder's epochMs
+         */
+        @JsonProperty("EpochMs")
+        private Long epochMs;
 
-		/**
-		 * The builder's transId
-		 */
-		@JsonProperty("TransID")
-		private String transId;
+        /**
+         * The builder's level
+         */
+        @JsonProperty("Level")
+        private String level;
 
-		/**
-		 * The builder's srcMethod
-		 */
-		@JsonProperty("SrcMethod")
-		private String srcMethod;
+        /**
+         * The builder's transId
+         */
+        @JsonProperty("TransID")
+        private String transId;
 
-		/**
-		 * The builder's srcLine
-		 */
-		@JsonProperty("SrcLine")
-		private Integer srcLine;
+        /**
+         * The builder's srcMethod
+         */
+        @JsonProperty("SrcMethod")
+        private String srcMethod;
+
+        /**
+         * The builder's srcLine
+         */
+        @JsonProperty("SrcLine")
+        private Integer srcLine;
 
         /**
          * The builder's tags
@@ -210,113 +218,126 @@ public class LogMsg {
         @JsonProperty("Tags")
         private List<String> tags;
 
-		/**
-		 * Sets the builder's msg
-		 * @param msg The msg to be set
-		 * @return Reference to the current object
-		 */
-		public Builder msg(final String msg) {
-		    this.msg = msg;
-		    return this;
-		}
+        public Builder id(final String id) {
+            this.id = id;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's data
-		 * @param data The data to be set
-		 * @return Reference to the current object
-		 */
-		public Builder data(final String data) {
-		    this.data = data;
-		    return this;
-		}
+        /**
+         * Sets the builder's msg
+         *
+         * @param msg The msg to be set
+         * @return Reference to the current object
+         */
+        public Builder msg(final String msg) {
+            this.msg = msg;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's ex
-		 * @param ex The ex to be set
-		 * @return Reference to the current object
-		 */
-		public Builder ex(final StackifyError ex) {
-		    this.ex = ex;
-		    return this;
-		}
+        /**
+         * Sets the builder's data
+         *
+         * @param data The data to be set
+         * @return Reference to the current object
+         */
+        public Builder data(final String data) {
+            this.data = data;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's th
-		 * @param th The th to be set
-		 * @return Reference to the current object
-		 */
-		public Builder th(final String th) {
-		    this.th = th;
-		    return this;
-		}
+        /**
+         * Sets the builder's ex
+         *
+         * @param ex The ex to be set
+         * @return Reference to the current object
+         */
+        public Builder ex(final StackifyError ex) {
+            this.ex = ex;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's epochMs
-		 * @param epochMs The epochMs to be set
-		 * @return Reference to the current object
-		 */
-		public Builder epochMs(final Long epochMs) {
-		    this.epochMs = epochMs;
-		    return this;
-		}
+        /**
+         * Sets the builder's th
+         *
+         * @param th The th to be set
+         * @return Reference to the current object
+         */
+        public Builder th(final String th) {
+            this.th = th;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's level
-		 * @param level The level to be set
-		 * @return Reference to the current object
-		 */
-		public Builder level(final String level) {
-		    this.level = level;
-		    return this;
-		}
+        /**
+         * Sets the builder's epochMs
+         *
+         * @param epochMs The epochMs to be set
+         * @return Reference to the current object
+         */
+        public Builder epochMs(final Long epochMs) {
+            this.epochMs = epochMs;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's transId
-		 * @param transId The transId to be set
-		 * @return Reference to the current object
-		 */
-		public Builder transId(final String transId) {
-		    this.transId = transId;
-		    return this;
-		}
+        /**
+         * Sets the builder's level
+         *
+         * @param level The level to be set
+         * @return Reference to the current object
+         */
+        public Builder level(final String level) {
+            this.level = level;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's srcMethod
-		 * @param srcMethod The srcMethod to be set
-		 * @return Reference to the current object
-		 */
-		public Builder srcMethod(final String srcMethod) {
-		    this.srcMethod = srcMethod;
-		    return this;
-		}
+        /**
+         * Sets the builder's transId
+         *
+         * @param transId The transId to be set
+         * @return Reference to the current object
+         */
+        public Builder transId(final String transId) {
+            this.transId = transId;
+            return this;
+        }
 
-		/**
-		 * Sets the builder's srcLine
-		 * @param srcLine The srcLine to be set
-		 * @return Reference to the current object
-		 */
-		public Builder srcLine(final Integer srcLine) {
-		    this.srcLine = srcLine;
-		    return this;
-		}
+        /**
+         * Sets the builder's srcMethod
+         *
+         * @param srcMethod The srcMethod to be set
+         * @return Reference to the current object
+         */
+        public Builder srcMethod(final String srcMethod) {
+            this.srcMethod = srcMethod;
+            return this;
+        }
+
+        /**
+         * Sets the builder's srcLine
+         *
+         * @param srcLine The srcLine to be set
+         * @return Reference to the current object
+         */
+        public Builder srcLine(final Integer srcLine) {
+            this.srcLine = srcLine;
+            return this;
+        }
 
         /**
          * Sets the builder's tags
+         *
          * @param tags A list of tags associated to this LogMsg
          * @return Reference to the current object
          */
-        public Builder tags(final List<String> tags)
-        {
+        public Builder tags(final List<String> tags) {
             this.tags = tags;
             return this;
         }
 
-
-		/**
-		 * @return A new object constructed from this builder
-		 */
-		public LogMsg build() {
-		    return new LogMsg(this);
-		}
-	}
+        /**
+         * @return A new object constructed from this builder
+         */
+        public LogMsg build() {
+            return new LogMsg(this);
+        }
+    }
 }
